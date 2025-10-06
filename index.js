aec8d4634808b48a27a7af0239b0d29be236c7b9
Moisés <moiseavilar0@gmail.com>
2025-10-06 09:14:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// Remove "/api" prefix se necessário
+server.use((req, res, next) => {
+  if (req.url.startsWith('/api')) {
+    req.url = req.url.substring(4);
+  }
+  next();
+});
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
